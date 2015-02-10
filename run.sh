388b1e098f39e600d03903d9bc9271623d238162
Takahiro Yano <speg03@gmail.com>
2015-02-10 08:16:15 +0000

    Add docker group

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 groupadd -g $GID $USER
-useradd -s /usr/bin/zsh -u $UID -g $GID $USER
+groupadd -g $DOCKER_GID docker
+useradd -s /usr/bin/zsh -u $UID -g $GID -G $DOCKER_GID $USER
 echo "$USER  ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers.d/$USER
 
 if [ $# = 0 ]; then
