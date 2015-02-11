79a826163126e1afeb746c88e46d03fcc73ebf77
Takahiro Yano <speg03@gmail.com>
2015-02-11 19:14:05 +0900

    Remove creation docker user

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-groupadd -g $DOCKER_GID docker
-useradd -M -s $(which nologin) -u $DOCKER_UID -g $DOCKER_GID docker
-
 groupadd -g $GID $USER
-useradd -d $SANDBOX_HOME -s $(which zsh) -u $UID -g $GID -G $DOCKER_GID $USER
+useradd -d $SANDBOX_HOME -s $(which zsh) -u $UID -g $GID $USER
 echo "$USER  ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers.d/$USER
 
 if [ $# = 0 ]; then
