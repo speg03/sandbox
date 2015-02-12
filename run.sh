8565d3435d3182e1e43ecd1c0e3ca51eb72d12b0
Takahiro Yano <speg03@gmail.com>
2015-02-12 13:04:21 +0900

    Add configuration for docker

--- run.sh
+++ run.sh
@@ -4,6 +4,14 @@ groupadd -g $GID $USER
 useradd -d $SANDBOX_HOME -s $(which zsh) -u $UID -g $GID $USER
 echo "$USER  ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers.d/$USER
 
+### Configuration for Docker
+if [ -e /var/run/docker.sock ]; then
+    docker_gid=$(stat -c %g /var/run/docker.sock)
+    groupadd -g $docker_gid docker
+    gpasswd -a $USER docker
+fi
+
+### Run sandbox
 if [ $# = 0 ]; then
     /usr/sbin/sshd -D
 else
