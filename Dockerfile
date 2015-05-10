b7bfc06009e64f2d4ecb7e517e86bfaed982aa46
Takahiro Yano <speg03@gmail.com>
2015-05-10 19:57:16 +0900

    minimize

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ MAINTAINER Takahiro Yano <speg03@gmail.com>
 RUN sed -ri 's/^(tsflags=nodocs)$/#\1/g' /etc/yum.conf
 
 # Install Packages
-RUN yum update -y && yum clean all
+RUN yum update -y --security && yum clean all
 ADD ./packages.list /build/
 RUN cat /build/packages.list | grep -v '^#' | grep -v '^$' \
     | xargs yum install -y && yum clean all
@@ -13,10 +13,6 @@ RUN cat /build/packages.list | grep -v '^#' | grep -v '^$' \
 # Set timezone to Asia/Tokyo
 RUN ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 
-# Configure SSH server
-RUN ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' \
-    && ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -N '' \
-    && ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ''
 EXPOSE 22
 
 ADD ./run.sh /build/
--- run.sh
+++ run.sh
@@ -1,27 +1,14 @@
 #!/bin/sh
 
 : ${SANDBOX_USER:=sandbox}
-: ${SANDBOX_UID:=1000}
-: ${SANDBOX_GID:=1000}
-: ${SANDBOX_HOME:=/home/sandbox}
 
-groupadd -g $SANDBOX_GID $SANDBOX_USER
-useradd -M -d $SANDBOX_HOME -s $(which zsh) -u $SANDBOX_UID -g $SANDBOX_GID $SANDBOX_USER
+useradd $SANDBOX_USER
 echo "$SANDBOX_USER  ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers.d/$SANDBOX_USER
 
-### Configuration for Docker
-if [ -e /var/run/docker.sock ]; then
-    docker_group_name=$(stat -c %G /var/run/docker.sock)
-    if [ "$docker_group_name" = "UNKNOWN" ]; then
-        docker_group_id=$(stat -c %g /var/run/docker.sock)
-        docker_group_name=docker
-        groupadd -g $docker_group_id $docker_group_name
-    fi
-    gpasswd -a $SANDBOX_USER $docker_group_name
-fi
-
-### Run sandbox
 if [ $# = 0 ]; then
+    ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' -q
+    ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -N '' -q
+    ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N '' -q
     /usr/sbin/sshd -De
 else
     runuser - $SANDBOX_USER -c "$*"
