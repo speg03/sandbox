530a8530cd2d35e932d91b59cd5609831a927c41
Takahiro Yano <speg03@gmail.com>
2021-01-12 10:43:32 +0900

    Change the encrypt algorhythm for SSH

--- workspace/build/run.sh
+++ workspace/build/run.sh
@@ -9,7 +9,7 @@ useradd -G wheel ${USERNAME} || true
 if [[ ! -e /home/${USERNAME} ]]; then
     mkdir -m 0700 /home/${USERNAME}
     mkdir -m 0700 /home/${USERNAME}/.ssh
-    ssh-keygen -t rsa -b 4096 -C "${USERNAME}@${HOSTNAME}" -N "" -f /home/${USERNAME}/.ssh/sandbox_${HOSTNAME}
+    ssh-keygen -t ed25519 -C "${USERNAME}@${HOSTNAME}" -N "" -f /home/${USERNAME}/.ssh/sandbox_${HOSTNAME}
     cp /home/${USERNAME}/.ssh/sandbox_${HOSTNAME}.pub /home/${USERNAME}/.ssh/authorized_keys
     chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}
 fi
